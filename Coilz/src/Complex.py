810682eff2d0c46cfc53d4d3cf3a84e429113176
Erwin Konink <erwinkonink@MacBook-Air-van-Erwin.local>
2017-07-09 23:57:14 +0200

    Implemented an add function on Complex

--- Coilz/src/Complex.py
+++ Coilz/src/Complex.py
@@ -2,3 +2,6 @@ class Complex:
     def __init__(self, realpart, imagpart):
         self.r = realpart
         self.i = imagpart
+
+    def add(self, c):
+        return Complex(self.r + c.r, self.i + c.i)
--- Coilz/tests/complexTests.py
+++ Coilz/tests/complexTests.py
@@ -13,3 +13,11 @@ class TestComplexNumber(unittest.TestCase):
         i = 1.7
         c = Complex(0, i)
         self.assertEqual(c.i, i)
+
+    def test_add(self):
+        c1 = Complex(1, 3)
+        c2 = Complex(7, 11)
+
+        c3 = c1.add(c2)
+        self.assertEqual(c3.r, 8)
+        self.assertEqual(c3.i, 14)
