7741170ffc0ac3dc1bc1a4b87d65094a865a841b
Erwin Konink <erwinkonink@gmail.com>
2017-07-10 09:46:30 +0200

    Added a list lesson

---
+++ Zilver/hallominkzilver.py
@@ -0,0 +1,22 @@
+print 'hallo Zilver'
+
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+myList.append(4)
+myList.append(5)
+print myList[0]
+print myList[2]
+print myList[4]
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+print "A list: %s" % myList
+
+astring = "Hello world!"
+print len(astring)
+print astring.index("o")
+print astring.count("l")
+print astring[3:7]
